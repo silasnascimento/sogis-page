d056f2c5cd2b6d2394c5205d14cf05db5bf32d9c
Silas Oliveira <silasogeo@Silass-MacBook-Pro.local>
2026-03-12 21:54:35 -0300

    Configura deploy no GitHub Pages via Actions

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/sogis-page",
+  images: {
+    unoptimized: true,
+  },
   /* config options here */
 };
 
